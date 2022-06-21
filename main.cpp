331aa37f9ec12bd10e77601227e2b292b2282608
diftucs <95053553+diftucs@users.noreply.github.com>
2022-06-21 13:53:47 +0200

    Use SDL to display stream instead of saving it

--- main.cpp
+++ main.cpp
@@ -15,9 +15,14 @@
 	along with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_thread.h>
+
 extern "C"
 {
 #include "libavcodec/avcodec.h"
+#include "libswscale/swscale.h"
 #include "libavdevice/avdevice.h"
 }
 
@@ -29,37 +34,102 @@ int main(int argc, char **argv)
 	avformat_open_input(&inputFormatContext, "rtmp://localhost/publishlive/livestream", NULL, NULL);
 	avformat_find_stream_info(inputFormatContext, NULL); // Will hang waiting for input
 
-	// Init the output AVFormatContext. Will contain the output AVStream
-	AVFormatContext *outputFormatContext;
-	avformat_alloc_output_context2(&outputFormatContext, NULL, "flv", NULL);
+	// Init an AVCodecContext with the codec needed to decode the input AVStream
+	AVCodecContext *inputCodecContext = avcodec_alloc_context3(avcodec_find_decoder(inputFormatContext->streams[0]->codecpar->codec_id));
+	// Apply the input AVStream's parameters to the codec
+	avcodec_parameters_to_context(inputCodecContext, inputFormatContext->streams[0]->codecpar);
+	// Initialize the input codec
+	avcodec_open2(inputCodecContext, NULL, NULL);
 
-	// Init the output AVStream
-	AVStream *outputStream = avformat_new_stream(outputFormatContext, NULL);
-	outputStream->codecpar = inputFormatContext->streams[0]->codecpar;
-
-	// Open the output stream
-	avio_open(&outputFormatContext->pb, "/tmp/saved.flv", AVIO_FLAG_WRITE);
-
-	// Write output header
-	avformat_write_header(outputFormatContext, NULL);
-
-	// Allocate memory for encoded pre- and post-converted packets
+	// Allocate memory needed for decoding
 	AVPacket *inputPacket = av_packet_alloc();
+	AVFrame *inputFrame = av_frame_alloc();
+	AVFrame *outputFrame = av_frame_alloc();
 
-	for (int i = 0; i < 250; i++)
+	// Allocate buffer for outputFrame since av_frame_alloc only allocates memory for the frame itself
+	outputFrame->format = inputCodecContext->pix_fmt;
+	outputFrame->width = inputCodecContext->width;
+	outputFrame->height = inputCodecContext->height;
+	av_frame_get_buffer(outputFrame, 0);
+
+	// Init SDL library
+	SDL_Init(SDL_INIT_VIDEO);
+	// SDL display window
+	SDL_Window *window = SDL_CreateWindow("Stream display", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, inputFormatContext->streams[0]->codecpar->width, inputFormatContext->streams[0]->codecpar->height, SDL_WINDOW_RESIZABLE);
+	// SDL rendering context
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	// What SDL draws on the window through the rendering context
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_YV12, SDL_TEXTUREACCESS_STREAMING, inputFormatContext->streams[0]->codecpar->width, inputFormatContext->streams[0]->codecpar->height);
+
+	// Init FFmpeg scaling to fit frames to the SDL texture
+	SwsContext *sws_ctx = sws_getContext(inputFormatContext->streams[0]->codecpar->width,
+										 inputFormatContext->streams[0]->codecpar->height,
+										 static_cast<AVPixelFormat>(inputFormatContext->streams[0]->codecpar->format),
+										 inputFormatContext->streams[0]->codecpar->width,
+										 inputFormatContext->streams[0]->codecpar->height,
+										 static_cast<AVPixelFormat>(inputFormatContext->streams[0]->codecpar->format),
+										 SWS_BILINEAR,
+										 NULL,
+										 NULL,
+										 NULL);
+
+	SDL_Event event;
+	while (1)
 	{
+		// Handle SDL window resizing
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type = SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+			{
+				// Resize the SDL texture to fit the SDL window
+				SDL_DestroyTexture(texture);
+				texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_YV12, SDL_TEXTUREACCESS_STATIC, event.window.data1, event.window.data2);
+
+				// Change the target resolution for the FFmpeg rescaler to fit the new SDL texture resolution
+				sws_ctx = sws_getCachedContext(sws_ctx,
+											   inputFormatContext->streams[0]->codecpar->width,
+											   inputFormatContext->streams[0]->codecpar->height,
+											   static_cast<AVPixelFormat>(inputFormatContext->streams[0]->codecpar->format),
+											   event.window.data1,
+											   event.window.data2,
+											   static_cast<AVPixelFormat>(inputFormatContext->streams[0]->codecpar->format),
+											   SWS_BILINEAR,
+											   NULL,
+											   NULL,
+											   NULL);
+			}
+		}
+
 		// Get packet from input
 		av_read_frame(inputFormatContext, inputPacket);
 
-		// Write packet to output
-		av_interleaved_write_frame(outputFormatContext, inputPacket);
+		// Send packet to decoder
+		avcodec_send_packet(inputCodecContext, inputPacket);
+		// Fetch frame from decoder and loop if more data is needed
+		if (avcodec_receive_frame(inputCodecContext, inputFrame) == AVERROR(EAGAIN))
+			continue;
+
+		// Scale frame with FFmpeg to fit the texture
+		sws_scale(sws_ctx, inputFrame->data, inputFrame->linesize, 0, inputFormatContext->streams[0]->codecpar->height, outputFrame->data, outputFrame->linesize);
+
+		// Draw the new data to the texture
+		SDL_UpdateYUVTexture(texture, NULL, outputFrame->data[0], outputFrame->linesize[0],
+							 outputFrame->data[1], outputFrame->linesize[1],
+							 outputFrame->data[2], outputFrame->linesize[2]);
+
+		// Clear the renderer's old data and copy the new texture to it's backbuffer
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+		// Apply the rendering context's backbuffer
+		SDL_RenderPresent(renderer);
+
+		// Draw the renderer's data onto the window
+		SDL_UpdateWindowSurface(window);
 
 		// Free the packet buffers since they are not reused
 		av_packet_unref(inputPacket);
 	}
-
-	// Write end of stream
-	av_write_trailer(outputFormatContext);
 
 	return 0;
 }
